178d6edada2285ffaf3ab833e2d6bed258c69218
Hakan Cem Gerçek <75271677+hakancemG@users.noreply.github.com>
2025-09-04 15:47:48 +0300

    VaccineRepository.java

--- src/main/java/com/veterinersistemi/randevu/repository/VaccineRepository.java
+++ src/main/java/com/veterinersistemi/randevu/repository/VaccineRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@Repository
 public interface VaccineRepository extends JpaRepository<Vaccine, Long> {
     List<Vaccine> findByPatientId(Long patientId);
 }
