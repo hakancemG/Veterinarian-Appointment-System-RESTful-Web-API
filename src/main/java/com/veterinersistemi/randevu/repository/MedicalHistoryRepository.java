113f17690f390d56a71a314d2bfefb28175f8c5c
Hakan Cem Gerçek <75271677+hakancemG@users.noreply.github.com>
2025-09-04 15:47:24 +0300

    MedicalHistoryRepository.java

--- src/main/java/com/veterinersistemi/randevu/repository/MedicalHistoryRepository.java
+++ src/main/java/com/veterinersistemi/randevu/repository/MedicalHistoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+@Repository
 public interface MedicalHistoryRepository extends JpaRepository<MedicalHistory, Long> {
     List<MedicalHistory> findByPatientId(Long patientId);
 }
