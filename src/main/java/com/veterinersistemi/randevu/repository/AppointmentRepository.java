7c31b78da025c769d7afe9ab858b3b8a4eb1bf66
Hakan Cem Gerçek <75271677+hakancemG@users.noreply.github.com>
2025-09-04 15:46:47 +0300

    AppointmentRepository.java

--- src/main/java/com/veterinersistemi/randevu/repository/AppointmentRepository.java
+++ src/main/java/com/veterinersistemi/randevu/repository/AppointmentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
     List<Appointment> findByDateAndVeterinarianId(LocalDateTime date, Long veterinarianId);
 }
