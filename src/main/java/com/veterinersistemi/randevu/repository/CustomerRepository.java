167580876417bdbc15def5498f29d19cb0bed87c
Hakan Cem Gerçek <75271677+hakancemG@users.noreply.github.com>
2025-09-04 15:47:11 +0300

    CustomerRepository.java

--- src/main/java/com/veterinersistemi/randevu/repository/CustomerRepository.java
+++ src/main/java/com/veterinersistemi/randevu/repository/CustomerRepository.java
@@ -3,5 +3,6 @@ package com.veterinersistemi.randevu.repository;
 import com.veterinersistemi.randevu.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
