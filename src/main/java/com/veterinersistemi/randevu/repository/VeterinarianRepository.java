642e24ba31376ca5bce8fa35db374a836a3a9265
Hakan Cem Gerçek <75271677+hakancemG@users.noreply.github.com>
2025-09-04 15:47:57 +0300

    VeterinarianRepository.java

--- src/main/java/com/veterinersistemi/randevu/repository/VeterinarianRepository.java
+++ src/main/java/com/veterinersistemi/randevu/repository/VeterinarianRepository.java
@@ -3,5 +3,6 @@ package com.veterinersistemi.randevu.repository;
 import com.veterinersistemi.randevu.entity.Veterinarian;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface VeterinarianRepository extends JpaRepository<Veterinarian, Long> {
 }
